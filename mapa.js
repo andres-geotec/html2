7f708f32b4234c731d061f241451f850fa362c17
andres-geotec <andres.geotec@gmail.com>
2025-11-24 13:02:38 -0600

    inicio del proyecto

---
+++ mapa.js
@@ -0,0 +1,13 @@
+
+new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+  ],
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
